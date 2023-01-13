813b5f19bacef4cdd2784dd15d27ceedab85de76
Volodya Kisil <volodymyrkisilv@gmail.com>
2023-01-13 08:22:49 +0200

    Connecting settings

--- news/progblog/news/admin.py
+++ news/progblog/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import News
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(News, NewsAdmin)
