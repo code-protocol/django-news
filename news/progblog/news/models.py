6eac5bf6ae62a073b98886062d9f8ca9687de44a
Volodya Kisil <volodymyrkisilv@gmail.com>
2023-01-13 08:45:44 +0200

    Creating a new 'Category' model

--- news/progblog/news/models.py
+++ news/progblog/news/models.py
@@ -8,9 +8,17 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     is_published = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     class Meta:
         verbose_name = 'News'
         verbose_name_plural = 'News'
         ordering = ['-created_at']
     
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Category', db_index=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
